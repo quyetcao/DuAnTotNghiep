9703ea454339ebafd9f60ea5ff1fe4b772f5356c
truongviethoang1 <tvhoang0803@gmail.com>
2024-09-22 16:24:00 +0700

    hoàn thành content

--- src/component/Pages/home.jsx
+++ src/component/Pages/home.jsx
@@ -2,24 +2,26 @@
 // import { Header } from "../common";
 // import { Footer } from "../common";
 import Banner from "../Banner/banner";
-import { Container, Box } from '@mui/material';
-import CustomerReviews from './Content/CustomerReviews';
-import FeaturesSection from './Content/FeaturesSection';
-import PartnersSection from './Content/PartnersSection';
-import StationsSection from './Content/StationsSection';
+import Content from "../Content/content";
+// import { Container, Box } from '@mui/material';
+// import CustomerReviews from './Content/CustomerReviews';
+// import FeaturesSection from './Content/FeaturesSection';
+// import PartnersSection from './Content/PartnersSection';
+// import StationsSection from './Content/StationsSection';
 
 export default function Home() {
     return (
         <div>
             <Banner />
-            <Container maxWidth="lg">
+            <Content/>
+            {/* <Container maxWidth="lg">
                 <Box mt={4} >
                     <CustomerReviews />
                     <FeaturesSection />
                     <PartnersSection />
                     <StationsSection />
                 </Box>
-            </Container >
+            </Container > */}
         </div>
 
     )
