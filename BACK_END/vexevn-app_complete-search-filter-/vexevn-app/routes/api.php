d164250ec125c8366d87b141021579cc058c496e
Baontin <tinvtpd07947@gmail.com>
2024-10-30 21:18:00 +0700

    refactor: remove middleware at api.php for testing features

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/routes/api.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/routes/api.php
@@ -19,100 +19,99 @@ Route::get('/user', function (Request $request) {
 ===========================================================================*/
 
 // CAR TYPE
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/cartypes/index', [CarController::class, 'listCarType']);
 
-    Route::post('/cartypes/create', [CarController::class, 'addCarType']);
+Route::get('/cartypes/index', [CarController::class, 'listCarType']);
 
-    Route::post('/cartypes/update/{id}', [CarController::class, 'updateCarType']);
+Route::post('/cartypes/create', [CarController::class, 'addCarType']);
 
-    Route::delete('/cartypes/delete/{id}', [CarController::class, 'deleteCarType']);
-});
+Route::post('/cartypes/update/{id}', [CarController::class, 'updateCarType']);
+
+Route::delete('/cartypes/delete/{id}', [CarController::class, 'deleteCarType']);
+
 
 // CAR HOUSE
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/carhouse/index', [CarController::class, 'listCarHouse']);
 
-    Route::post('/carhouse/create', [CarController::class, 'addCarHouse']);
+Route::get('/carhouse/index', [CarController::class, 'listCarHouse']);
 
-    Route::post('/carhouse/update/{id}', [CarController::class, 'updateCarHouse']);
+Route::post('/carhouse/create', [CarController::class, 'addCarHouse']);
 
-    Route::delete('/carhouse/delete/{id}', [CarController::class, 'deleteCarHouse']);
-});
+Route::post('/carhouse/update/{id}', [CarController::class, 'updateCarHouse']);
+
+Route::delete('/carhouse/delete/{id}', [CarController::class, 'deleteCarHouse']);
+
 
 // CAR
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/car/index', [CarController::class, 'listCar']);
 
-    Route::post('/car/create', [CarController::class, 'uploadCar']);
+Route::get('/car/index', [CarController::class, 'listCar']);
 
-    Route::post('/car/update/{id}', [CarController::class, 'updateCar']);
+Route::post('/car/create', [CarController::class, 'uploadCar']);
 
-    Route::delete('car/delete/{id}', [CarController::class, 'deleteCar']);
-});
+Route::post('/car/update/{id}', [CarController::class, 'updateCar']);
+
+Route::delete('car/delete/{id}', [CarController::class, 'deleteCar']);
+
 
 // CITY
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/city/index', [CarController::class, 'listCity']);
 
-    Route::post('/city/create', [CarController::class, 'addCity']);
+Route::get('/city/index', [CarController::class, 'listCity']);
 
-    Route::post('/city/update/{id}', [CarController::class, 'updateCity']);
+Route::post('/city/create', [CarController::class, 'addCity']);
 
-    Route::delete('/city/delete/{id}', [CarController::class, 'deleteCity']);
-});
+Route::post('/city/update/{id}', [CarController::class, 'updateCity']);
+
+Route::delete('/city/delete/{id}', [CarController::class, 'deleteCity']);
+
 
 // PICK UP POINT
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/pickuppoint/index', [CarController::class, 'listPickupPoint']);
 
-    Route::post('/pickuppoint/create', [CarController::class, 'uploadPickupPoint']);
+Route::get('/pickuppoint/index', [CarController::class, 'listPickupPoint']);
 
-    Route::post('/pickuppoint/update/{id}', [CarController::class, 'updatePickupPoint']);
+Route::post('/pickuppoint/create', [CarController::class, 'uploadPickupPoint']);
 
-    Route::delete('/pickuppoint/delete/{id}', [CarController::class, 'deletePickupPoint']);
+Route::post('/pickuppoint/update/{id}', [CarController::class, 'updatePickupPoint']);
 
-    // Route::get('')
-});
+Route::delete('/pickuppoint/delete/{id}', [CarController::class, 'deletePickupPoint']);
+
+// Route::get('')
+
 
 // DROP OFF POINT
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/dropoffpoint/index', [CarController::class, 'listDropoffPoint']);
 
-    Route::post('/dropoffpoint/create', [CarController::class, 'uploadDropoffPoint']);
+Route::get('/dropoffpoint/index', [CarController::class, 'listDropoffPoint']);
 
-    Route::post('/dropoffpoint/update/{id}', [CarController::class, 'updateDropoffPoint']);
+Route::post('/dropoffpoint/create', [CarController::class, 'uploadDropoffPoint']);
 
-    Route::delete('/dropoffpoint/delete/{id}', [CarController::class, 'deleteDropoffPoint']);
+Route::post('/dropoffpoint/update/{id}', [CarController::class, 'updateDropoffPoint']);
 
-    // Route::get('')
-});
+Route::delete('/dropoffpoint/delete/{id}', [CarController::class, 'deleteDropoffPoint']);
+
+// Route::get('')
+
 
 Route::get('/provinces', [ProvinceController::class, 'getProvinces']);
 
 // CAR ROUTE POINT
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/carroute/index', [CarController::class, 'listCarRoute']);
 
-    Route::post('/carroute/create', [CarController::class, 'uploadCarRoute']);
+Route::get('/carroute/index', [CarController::class, 'listCarRoute']);
 
-    Route::post('/carroute/update/{id}', [CarController::class, 'updateCarRoute']);
+Route::post('/carroute/create', [CarController::class, 'uploadCarRoute']);
 
-    Route::delete('/carroute/delete/{id}', [CarController::class, 'deleteCarRoute']);
-});
+Route::post('/carroute/update/{id}', [CarController::class, 'updateCarRoute']);
+
+Route::delete('/carroute/delete/{id}', [CarController::class, 'deleteCarRoute']);
+
 
 // CAR TRIP
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/cartrip/index', [CarTripController::class, 'listCarTrip']);
 
-    Route::post('/cartrip/create', [CarTripController::class, 'uploadCarTrip']);
+Route::get('/cartrip/index', [CarTripController::class, 'listCarTrip']);
 
-    Route::post('/cartrip/update/{id}', [CarTripController::class, 'updateCarTrip']);
+Route::post('/cartrip/create', [CarTripController::class, 'uploadCarTrip']);
 
-    Route::delete('/cartrip/delete/{id}', [CarTripController::class, 'deleteCarTrip']);
-});
+Route::post('/cartrip/update/{id}', [CarTripController::class, 'updateCarTrip']);
+
+Route::delete('/cartrip/delete/{id}', [CarTripController::class, 'deleteCarTrip']);
+
 
 // SEARCH
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/cartrip/search', [SearchController::class, 'searchCarTrip']);
-});
+
+Route::get('/cartrip/search', [SearchController::class, 'searchCarTrip']);
