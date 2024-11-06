f62ab9b1dd5cab14d9f52b03eedaabe65ee225d4
tafuwi <130528687+tafuwi@users.noreply.github.com>
2024-11-06 11:27:33 +0700

    change string image ->file

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/database/migrations/2024_11_01_090431_create_banners_table.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/database/migrations/2024_11_01_090431_create_banners_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('banners', function (Blueprint $table) {
             $table->id();
-            $table->string('image'); 
+            $table->file('image'); 
             $table->string('alt_text')->nullable(); 
             $table->timestamps();
         });
