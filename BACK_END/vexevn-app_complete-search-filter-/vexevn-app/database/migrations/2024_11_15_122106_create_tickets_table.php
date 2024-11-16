2708e5bb92c8e6ad37f64ebfe87da865985c7f46
tafuwi <130528687+tafuwi@users.noreply.github.com>
2024-11-16 15:24:54 +0700

    test

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/database/migrations/2024_11_15_122106_create_tickets_table.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/database/migrations/2024_11_15_122106_create_tickets_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,16 +13,20 @@ class CreateTicketsTable extends Migration
     public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
-            $table->id(); // ID vé
-            $table->string('name');//tên vé
-            $table->unsignedBigInteger('car_id'); // Liên kết với xe
-            $table->unsignedBigInteger('car_trip_id'); // Liên kết với chuyến xe
-            $table->enum('status', ['not_started','running','completed']); // Trạng thái vé
+            $table->id(); 
+            $table->string('name');
+            $table->enum('status', ['not_started', 'running', 'completed']); 
+            $table->unsignedBigInteger('car_trip_id')->nullable(); 
+            $table->unsignedBigInteger('car_id');
+            $table->unsignedBigInteger('car_route_id')->nullable();
             $table->timestamps();
 
-            // Thiết lập khóa ngoại cho car_trip_id để liên kết với bảng car_trips
+            // Ràng buộc khóa ngoại với cascade delete
+            $table->foreign('car_trip_id')->references('id')->on('car_trips')->onDelete('cascade'); 
             $table->foreign('car_id')->references('id')->on('cars')->onDelete('cascade');
-            $table->foreign('car_trip_id')->references('id')->on('car_trips')->onDelete('cascade');
+            $table->foreign('car_route_id')->references('id')->on('car_routes')->onDelete('cascade');   
+                  
+                  
         });
     }
 
@@ -34,8 +37,11 @@ class CreateTicketsTable extends Migration
      */
     public function down()
     {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropForeign(['car_trip_id']); // Xóa ràng buộc khóa ngoại
+        });
+
         Schema::dropIfExists('tickets');
     }
 }
-
 
