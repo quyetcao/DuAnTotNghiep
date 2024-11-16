dd17b67d30d4b2406697dc4489e450e885981dc4
tafuwi <130528687+tafuwi@users.noreply.github.com>
2024-11-16 15:26:10 +0700

    test

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Http/Controllers/Api/TicketController.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Http/Controllers/Api/TicketController.php
@@ -4,16 +4,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Ticket;
-use App\Models\CarType;
-use App\Models\CarHouse;
-use App\Models\Car;
-use App\Models\CarImage;
-use App\Models\PickupPoint;
-use App\Models\DropoffPoint;
-use App\Models\BusRoute;
-use App\Models\CarTrip;
-use App\Models\CarTripDropoffPoint;
-use App\Models\CarTripPickupPoint;
 use Illuminate\Support\Facades\Validator;
 
 class TicketController extends HelpController
@@ -21,48 +11,65 @@ class TicketController extends HelpController
     // 1. Hiển thị thông tin vé theo ID
     public function showTicket($id)
     {
-        $data = Ticket::find($id);
-
+        $data = Ticket::with(['carTrip','car','pickupPoints', 'dropoffPoints'])->find($id);
+    
         if (!$data) {
             return response()->json(['message' => 'Vé không tồn tại'], 404);
         }
-
+    
         return response()->json($data);
     }
+    
 
     // 2. Danh sách tất cả vé
     public function listTicket()
     {
-        $data = Ticket::all(); //show 10 vé
+        $data = Ticket::with(['carTrip','car','pickupPoints', 'dropoffPoints'])->paginate(10);
+    
+        if ($data->isEmpty()) { //isEmpty kiểm tra data
+            return response()->json([
+                'status' => 404,
+                'message' => 'Kho vé đang trống!'
+            ], 404);
+        }
+    
         return $this->sendResponse(
-        200, 'Hiển thị danh sách vé thành công', $data);
-
+            200, 'Hiển thị danh sách vé thành công', $data
+        );
     }
+    
 
     // 3. Tạo vé mới
     public function createTicket(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'car_trip_id' => 'required|exists:car_trips,id',
-            'price' => 'required|numeric',
-            'seat_number' => 'required|string',
+            'name' => 'required|string',
             'status' => 'required|in:not_started,running,completed',
-        ]);
+            'car_trip_id' => 'nullable|exists:car_trips,id',
+            'car_id' => 'nullable|exists:cars,id',
+            'car_route_id' => 'nullable|exists:car_routes,id',
 
+            'pickup_points.*.id' => 'required|exists:pickup_points,id',
+            'pickup_points.*.pickup_time' => 'required|date_format:H:i',
+            'dropoff_points.*.id' => 'required|exists:dropoff_points,id',
+            'dropoff_points.*.dropoff_time' => 'required|date_format:H:i',
+        ]);
+    
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
-
-        $ticket = new Ticket();
-        $ticket->car_trip_id = $request->car_trip_id;
-        $ticket->price = $request->price;
-        $ticket->seat_number = $request->seat_number;
-        $ticket->status = $request->status;
-        $ticket->save();
-
+    
+        $ticket = Ticket::create([
+            'name' => $request->name,
+            'status' => $request->status,
+            'car_trip_id' => $request->car_trip_id,
+            'car_id' => $request->car_id,
+            'car_route_id' => $request->car_route_id,
+        ]);
+    
         return response()->json(['message' => 'Tạo vé thành công', 'ticket' => $ticket], 201);
     }
-
+    
     // 4. Cập nhật thông tin vé
     public function updateTicket(Request $request, $id)
     {
@@ -73,7 +80,7 @@ class TicketController extends HelpController
         }
 
         $validator = Validator::make($request->all(), [
-            'price' => 'nullable|numeric',
+            'name' => 'nullable|string',
             'status' => 'nullable|in:not_started,running,completed',
         ]);
 
@@ -81,8 +88,8 @@ class TicketController extends HelpController
             return response()->json($validator->errors(), 400);
         }
 
-        if ($request->has('price')) {
-            $data->price = $request->price;
+        if ($request->has('name')) {
+            $data->name = $request->name;
         }
         if ($request->has('status')) {
             $data->status = $request->status;
