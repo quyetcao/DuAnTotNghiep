7cfabd7861b96594ec966237c907454490068305
tafuwi <130528687+tafuwi@users.noreply.github.com>
2024-11-16 15:25:40 +0700

    test

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Models/Ticket.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Models/Ticket.php
@@ -9,8 +9,47 @@ class Ticket extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'cars_id',
+        'name',
         'status',
+        'car_trip_id',
+        'car_id',
+        'car_route_id'
     ];
-   
+    
+    public function carTrip()
+    {
+        return $this->belongsTo(CarTrip::class);
+    }
+    public function car() {
+        return $this->belongsTo(Car::class);
+    }
+
+    public function carRoute() {
+        return $this->belongsTo(CarRoute::class);
+    }
+
+    public function pickupPoints() {
+        return $this->belongsToMany(PickupPoint::class, 'car_trip_pickup_points')
+                    ->withPivot('pickup_time')
+                    ->withTimestamps();
+    }
+
+    public function dropoffPoints() {
+        return $this->belongsToMany(DropoffPoint::class, 'car_trip_dropoff_points')
+                    ->withPivot('dropoff_time')
+                    ->withTimestamps();
+        
+    }
+
+    public function scopeNotStarted($query) {
+        return $query->where('status', 'not_started');
+    }
+    
+    public function scopeRunning($query) {
+        return $query->where('status', 'running');
+    }
+
+    public function scopeCompleted($query) {
+        return $query->where('status', 'completed');
+    }
 }
