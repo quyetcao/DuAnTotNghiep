7520e9289986355430bea375fbba842d26905271
tafuwi <taidemons12224@gmail.com>
2024-11-19 15:53:19 +0700

    V4 random name

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Http/Controllers/Api/TicketController.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Http/Controllers/Api/TicketController.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Ticket;
 use Illuminate\Support\Facades\Validator;
-
+use Illuminate\Support\Str; //string ramdom
 class TicketController extends HelpController
 {
     // 1. Hiển thị thông tin vé theo ID
@@ -56,8 +56,11 @@ class TicketController extends HelpController
     // 3. Tạo vé mới
     public function createTicket(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string',
+        //name tạo ngẫu nhiên
+        $name = $request->has('name') ? $request->name : Str::random(10);
+        
+          $validator = Validator::make($request->all(), [
+            'name' => 'nullable|string|unique:tickets,name,' . $name,
             'status' => 'required|in:not_started,running,completed',
             'car_trip_id' => 'nullable|exists:car_trips,id',
             'car_id' => 'nullable|exists:cars,id',
@@ -65,16 +68,15 @@ class TicketController extends HelpController
             'seat_car_trips_id' => 'nullable|exists:seat_car_trips,id',
             'pickup_points_id' => 'nullable|exists:pickup_points,id',
             'dropoff_points_id' => 'nullable|exists:dropoff_points,id',
-            
-        ]);
-    
+            ]);
+           
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
     
         // Tạo vé mới
         $ticket = Ticket::create([
-            'name' => $request->name,
+            'name' => $name,
             'status' => $request->status,
             'car_trip_id' => $request->car_trip_id,
             'car_id' => $request->car_id,
@@ -107,22 +109,35 @@ class TicketController extends HelpController
         if (!$data) {
             return response()->json(['message' => 'Vé không tồn tại'], 404);
         }
-
+        //tạo name ngẫu nhiên
+       
+        $name = $request->has('name') ? $request->name : Str::random(10);
         $validator = Validator::make($request->all(), [
-            'name' => 'nullable|string',
-            'status' => 'nullable|in:not_started,running,completed',
+           'name' => 'nullable|string|unique:tickets,name,' . $name,
+            'status' => 'required|in:not_started,running,completed',
+            'car_trip_id' => 'nullable|exists:car_trips,id',
+            'car_id' => 'nullable|exists:cars,id',
+            'car_route_id' => 'nullable|exists:car_routes,id',
+            'seat_car_trips_id' => 'nullable|exists:seat_car_trips,id',
+            'pickup_points_id' => 'nullable|exists:pickup_points,id',
+            'dropoff_points_id' => 'nullable|exists:dropoff_points,id',
+            
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
-        if ($request->has('name')) {
-            $data->name = $request->name;
-        }
-        if ($request->has('status')) {
-            $data->status = $request->status;
-        }
+        $data->update([
+            'name' => $name,
+            'status' => $request->status,
+            'car_trip_id' => $request->car_trip_id,
+            'car_id' => $request->car_id,
+            'car_route_id' => $request->car_route_id,
+            'seat_car_trips_id' => $request->seat_car_trips_id,
+            'pickup_points_id' => $request->pickup_points_id, 
+            'dropoff_points_id' => $request->dropoff_points_id, 
+        ]);
 
         $data->save();
 
