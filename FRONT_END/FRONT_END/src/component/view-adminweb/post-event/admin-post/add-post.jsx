1570958cbe7438dcd64be960beeea6f1a59284ee
quyetcao <quyetcao200404@example.com>
2024-11-27 13:50:14 +0700

    front-frontend

--- FRONT_END/FRONT_END/src/component/view-adminweb/post-event/admin-post/add-post.jsx
+++ FRONT_END/FRONT_END/src/component/view-adminweb/post-event/admin-post/add-post.jsx
@@ -8,74 +8,86 @@ import { CallapiGetAllTitleEvent, CallapiPostArticles } from '../../../../redux/
 import { useEffect } from 'react';
 
 
-export default function AddEArticles(){
+export default function AddEArticles() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const notify = (event) => {
-        if(event == true){
+        if (event == true) {
             toast.success("Thêm Thành Công!", { theme: "colored" });
-        }else if(event == false){
+        } else if (event == false) {
             toast.error("Lỗi form nhập!", { theme: "colored" });
         }
     }
-   
-    useEffect(()=>{
-        dispatch(CallapiGetAllTitleEvent()) 
-    },[])
+
+    useEffect(() => {
+        dispatch(CallapiGetAllTitleEvent())
+    }, [])
     const dataTitleEvent = useSelector((state) => state.StoreEventPost?.dataTitleEvent);
     const { register, handleSubmit } = useForm()
     const onSubmit = (data) => {
-        console.log("data form bài viết",data);
-        dispatch(CallapiPostArticles(data))
+        console.log("data form bài viết", data);
+        const imageFiles = data.images;
+        const formData = new FormData();
+        formData.append('title', data.title); 
+        formData.append('event_id', data.event_id); 
+        formData.append('content', data.content); 
+        formData.append('publication_date', data.publication_date); 
+        formData.append('status', data.status);
+        for (let i = 0; i < imageFiles.length; i++) {
+            formData.append('images[]', imageFiles[i]);
+        }
+        dispatch(CallapiPostArticles(formData))
     }
     const isToastOk = useSelector((state) => state.StoreArticles?.popupXacNhan);
     const isToastError = useSelector((state) => state.StoreArticles?.popupError);
-   
+
     if (isToastOk === true) {
         notify(true);
         setTimeout(() => {
             navigate('/adminweb/listArticles');
         }, 2000);
-    } 
-    if(isToastError){
+    }
+    if (isToastError) {
         notify(false);
     }
-    
-    
-    
-    
-    return (
-         
-        <>
-          <ToastContainer/>
-        <h3 style={{textAlign:'center'}}>THÊM BÀI VIẾT</h3>
-            <div className="page-add-carhouse">
-            <form id="busForm" onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="title">Tên Bài Viết </label>
-                <input type="text" id="title"    {...register('title', { required: true })} placeholder="Vé Tết 2025..." />
 
-                <label htmlFor="title">Nhóm Sự Kiện </label>
-                <select name="event_id" id="event_id"  {...register('event_id')}>
-                   {dataTitleEvent && dataTitleEvent.map((item)=>{
-                    return <>
-                    <option value={item.id}>{item.title}</option>
-                    </>
-                   })} 
-                   
-                </select>
-                <label htmlFor="content">Nội dung bài viết</label>
-                <input type="text" id="content"    {...register('content', { required: true })} />
-                <label htmlFor="publication_date">Ngày Bắt Đầu</label>
-                <input type="date" id="publication_date"    {...register('publication_date')}  />
-                <label htmlFor="status">Trạng Thái</label>
-                <select name="status" id="status"  {...register('status')}>
-                    <option value="published">Hoạt Động</option>
-                    <option value="draft">Nháp</option>
-                </select>
-                <input type="submit" className='btnsb' value='Thêm Sự Kiện' />
-            </form>
+
+
+
+    return (
+
+        <>
+            <ToastContainer />
+            <h3 style={{ textAlign: 'center' }}>THÊM BÀI VIẾT</h3>
+            <div className="page-add-carhouse">
+                <form id="busForm" onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+                    <label htmlFor="title">Tên Bài Viết </label>
+                    <input type="text" id="title"    {...register('title', { required: true })} placeholder="Vé Tết 2025..." />
+
+                    <label htmlFor="title">Nhóm Sự Kiện </label>
+                    <select name="event_id" id="event_id"  {...register('event_id')}>
+                        {dataTitleEvent && dataTitleEvent.map((item) => {
+                            return <>
+                                <option value={item.id}>{item.title}</option>
+                            </>
+                        })}
+
+                    </select>
+                    <label htmlFor="content">Nội dung bài viết</label>
+                    <input type="text" id="content"    {...register('content', { required: true })} />
+                    <label htmlFor="publication_date">Ngày Bắt Đầu</label>
+                    <input type="date" id="publication_date"    {...register('publication_date')} />
+                    <label htmlFor="status">Trạng Thái</label>
+                    <select name="status" id="status"  {...register('status')}>
+                        <option value="published">Hoạt Động</option>
+                        <option value="draft">Nháp</option>
+                    </select>
+                    <label htmlFor="image-upload">Chọn ảnh:</label>
+                    <input type="file" id="image-upload" {...register('images')} name="images" accept="image/*" multiple />
+                        <input type="submit" className='btnsb' value='Thêm Sự Kiện' />
+                </form>
             </div>
-        
+
         </>
     )
 }
