f3c602587f42fa6c685d48f5d34d7fee54ac89c7
vthien243 <vubui11114@gmail.com>
2024-12-19 10:14:48 +0700

    Update Order

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Models/Order.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/app/Models/Order.php
@@ -100,4 +100,9 @@ class Order extends Model
     {
         return $this->carTrip->dropoffPoints();
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
