0df86fd86b41c528d660aa86ad755866078d518a
quyetcao <quyetcao200404@example.com>
2024-12-19 21:16:56 +0700

    deloys

---
+++ FRONT_END/FRONT_END/src/redux/city/Asynthunk_city.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { getallCity } from "./CreateSlice_city";
+
+
+
+// call api lấy thông tin chuyến xe khi nhận props từ ds cx
+export function callApiGetCity(){
+  return async (dispatch) => {
+    try {
+   
+      let res = await axios.get(`http://127.0.0.1:8000/api/cities`);
+      dispatch(getallCity(res.data));
+
+    } catch (error) {
+      console.log(error);
+    } 
+  };
+}
+
