878d86801ff40aa2bbf3c610fb9c3f80c5b11282
tafuwi <130528687+tafuwi@users.noreply.github.com>
2024-12-26 00:18:18 +0700

    update services google redirect

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/config/services.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/config/services.php
@@ -38,8 +38,10 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
+
+
 
     'momo' => [
         'partner_code' => env('MOMO_PARTNER_CODE'),
