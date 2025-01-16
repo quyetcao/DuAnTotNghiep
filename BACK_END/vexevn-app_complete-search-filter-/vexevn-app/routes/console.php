3f619112ecd08ed1bf9a976a966d3950967f7a6b
Baontin <tinvtpd07947@gmail.com>
2025-01-16 23:48:40 +0700

    feat: update

--- BACK_END/vexevn-app_complete-search-filter-/vexevn-app/routes/console.php
+++ BACK_END/vexevn-app_complete-search-filter-/vexevn-app/routes/console.php
@@ -9,14 +9,15 @@ Artisan::command('inspire', function () {
   $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+Artisan::command('reset:completed-trips', function () {
+  CarTrip::resetCompletedTrips();
+  $this->info('Reset chuyến xe thành công!');
+})->describe('Reset trạng thái các chuyến xe đã completed về not_started, cập nhật ngày khởi hành và reset ghế.')
+  ->everyMinute(); // Gắn lịch trình trực tiếp
+
 Artisan::command('update:status-trips', function () {
   CarTrip::updateStatuses();
   $this->info('Cập nhật trạng thái chuyến xe thành công!');
 })->describe('Cập nhật trạng thái của các chuyến xe.')
   ->everyMinute(); // Gắn lịch trình trực tiếp
 
-Artisan::command('reset:completed-trips', function () {
-  CarTrip::resetCompletedTrips();
-  $this->info('Reset chuyến xe thành công!');
-})->describe('Reset trạng thái các chuyến xe đã completed về not_started, cập nhật ngày khởi hành và reset ghế.')
-  ->everyMinute(); // Gắn lịch trình trực tiếp
